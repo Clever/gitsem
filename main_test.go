367f8d08986ed29ee3322ae854310e2f5b27b310
Joseph Weigl <joseph.weigl@gmail.com>
2018-05-14 21:05:31 +0200

    Add tests

--- main_test.go
+++ main_test.go
@@ -7,22 +7,33 @@ import (
 )
 
 var bumpTests = []struct {
-	Old    string
-	New    string
-	Change string
+	Old          string
+	New          string
+	Change       string
+	isPreRelease bool
 }{
-	{Old: "1.0.0", New: "2.0.0", Change: "major"},
-	{Old: "1.1.0", New: "2.0.0", Change: "major"},
-	{Old: "1.1.1", New: "2.0.0", Change: "major"},
-	{Old: "1.0.0", New: "1.1.0", Change: "minor"},
-	{Old: "1.0.1", New: "1.1.0", Change: "minor"},
-	{Old: "1.1.1", New: "1.1.2", Change: "patch"},
+	{Old: "1.0.0", New: "2.0.0", Change: "major", isPreRelease: false},
+	{Old: "1.1.0", New: "2.0.0", Change: "major", isPreRelease: false},
+	{Old: "1.1.1", New: "2.0.0", Change: "major", isPreRelease: false},
+	{Old: "1.0.0", New: "1.1.0", Change: "minor", isPreRelease: false},
+	{Old: "1.0.1", New: "1.1.0", Change: "minor", isPreRelease: false},
+	{Old: "1.1.1", New: "1.1.2", Change: "patch", isPreRelease: false},
+
+	{Old: "1.0.0", New: "2.0.0-1", Change: "major", isPreRelease: true},
+	{Old: "1.1.0", New: "2.0.0-1", Change: "major", isPreRelease: true},
+	{Old: "1.1.1", New: "2.0.0-1", Change: "major", isPreRelease: true},
+	{Old: "1.0.0", New: "1.1.0-1", Change: "minor", isPreRelease: true},
+	{Old: "1.0.1", New: "1.1.0-1", Change: "minor", isPreRelease: true},
+	{Old: "1.1.1", New: "1.1.2-1", Change: "patch", isPreRelease: true},
+	{Old: "1.0.0-1", New: "1.0.0-2", Change: "major", isPreRelease: true},
+	{Old: "1.1.1-1", New: "1.1.1-2", Change: "minor", isPreRelease: true},
+	{Old: "1.1.1-1", New: "1.1.1-2", Change: "patch", isPreRelease: true},
 }
 
 func TestBump(t *testing.T) {
 	for _, test := range bumpTests {
 		v, err := semver.New(test.Old)
 		assert.Nil(t, err)
-		assert.Equal(t, test.New, bump(v, test.Change).String())
+		assert.Equal(t, test.New, bump(v, test.Change, test.isPreRelease).String())
 	}
 }
